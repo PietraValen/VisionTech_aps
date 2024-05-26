71dc71b64739b8804888318190269f9a0bf74e77
Spes-Tech <100318524+PietraValen@users.noreply.github.com>
2024-05-25 23:09:28 -0300

    Novas atualizações do projeto

--- js/script.js
+++ js/script.js
@@ -1,37 +1,122 @@
-/* Show Menu */
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const bars = document.querySelector(".bar"),
+  close = document.querySelector(".close"),
+  menu = document.querySelector(".menu");
 
-/* Menu show */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+bars.addEventListener("click", () => {
+  menu.classList.add("active");
+  gsap.from(".menu", {
+    opacity: 0,
+    duration: 0.3,
+  });
+
+  gsap.from(".menu ul", {
+    opacity: 0,
+    x: -300,
+  });
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+function animateContent(selector) {
+  selector.forEach((selector) => {
+    gsap.to(selector, {
+      y: 30,
+      duration: 0.1,
+      opacity: 1,
+      delay: 0.2,
+      stagger: 0.2,
+      ease: "power2.out",
+    });
   });
 }
 
-/* Menu hidden */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+function scrollTirggerAnimation(triggerSelector, boxSelectors) {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerSelector,
+      start: "top 50%",
+      end: "top 80%",
+      scrub: 1,
+    },
+  });
+
+  boxSelectors.forEach((boxSelector) => {
+    timeline.to(boxSelector, {
+      y: 0,
+      duration: 1,
+      opacity: 1,
+    });
   });
 }
 
-/* Remove menu Mobile*/
-const navLink = document.querySelectorAll(".nav__link");
+function swipeAnimation(triggerSelector, boxSelectors) {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerSelector,
+      start: "top 50%",
+      end: "top 100%",
+      scrub: 3,
+    },
+  });
 
-const linkAction = () => {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
-};
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+  boxSelectors.forEach((boxSelector) => {
+    timeline.to(boxSelector, {
+      x: 0,
+      duration: 1,
+      opacity: 1,
+    });
+  });
+}
 
-/* Change Backgorund Header*/
-const blurHeader = () => {
-  const header = document.getElementById("header");
-  // Add a class if the bottom offset is greater than 50 of the viewport
-  this.scrollY >= 50
-    ? header.classList.add("blur-header")
-    : header.classList.remove("blur-header");
-};
-window.addEventListener("scroll", blurHeader);
+function galleryAnimation(triggerSelector, boxSelectors) {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerSelector,
+      start: "top 100%",
+      end: "bottom 100%",
+      scrub: 1,
+    },
+  });
+
+  boxSelectors.forEach((boxSelector) => {
+    timeline.to(boxSelector, {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+    });
+  });
+}
+
+animateContent([
+  ".home .content h5, .home .content h1, .home .content p, .home .content .search",
+]);
+
+scrollTirggerAnimation(".esg", [".esg .box1", ".esg .box2", ".esg .box3"]);
+
+scrollTirggerAnimation(".feedback .container", [
+  ".feedback .label",
+  ".feedback .heading",
+  ".feedback .paragraph",
+]);
+
+scrollTirggerAnimation(".article", [".article .label", ".article .heading"]);
+
+swipeAnimation(".consultancy", [
+  ".consultancy .heading",
+  ".consultancy .content",
+]);
+
+swipeAnimation(".article", [
+  ".article .latest-article",
+  ".article .box1",
+  ".article .box2",
+  ".article .box3",
+  ".article .box4",
+]);
+
+galleryAnimation(".consultancy .gallery", [
+  ".consultancy .gallery .box1",
+  ".consultancy .gallery .box2",
+]);
